3200d2cfce423a2ea509eaf8e5b3e678d7a6117c
JoaquinCostamagna <jncostamagna@gmail.com>
2024-04-21 10:29:47 -0300

    MongoDb local para desarrollo

--- api-server/src/connections/mongoDb.ts
+++ api-server/src/connections/mongoDb.ts
@@ -1,15 +1,20 @@
 import mongoose from 'mongoose';
-
+import { connectToLocalMongo } from './mongoDbLocalServer';
 /**
  * The connection string for MongoDB.
  */
 const connectionString: string = process.env.ME_CONFIG_MONGODB_URL as string;
 
 console.log('Connecting to DataBase...');
-mongoose.connect(connectionString, {})
-    .then((db) => { 
-        console.log(`DataBase conenction established with ${db.connection.host}`);
-    })
-    .catch((error) => { console.error(`Connection Error: ${error}`) });
 
+if (!connectionString) {
+    connectToLocalMongo();
+}
+else {
+    mongoose.connect(connectionString, {})
+        .then((db) => {
+            console.log(`DataBase conenction established with ${db.connection.host}`);
+        })
+        .catch((error) => { console.error(`Connection Error: ${error}`) });
+}
 export const db = mongoose.connection;
